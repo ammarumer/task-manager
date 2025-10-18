21219bd511e6868a6cd660edcc169dd0c4e863c0
Ammar Umer <ammar@eniture-dev.com>
2025-10-18 09:57:41 +0500

    adds the logout and fixes issue on refresh

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
 
+import { protect as auth } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+// ✅ Verify token and return user info
+router.get("/me", auth, (req, res) => {
+    res.json({ email: req.user.email, id: req.user.id });
+});
 
 export default router;
